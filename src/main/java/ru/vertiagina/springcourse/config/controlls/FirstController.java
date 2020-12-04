77029a4e6ee1ba19595823cf825aec0ab807b182
Irina Vertiagina <irkinson9@gmail.com>
2020-12-04 15:28:59 -0500

    request param name add - print in console

--- src/main/java/ru/vertiagina/springcourse/config/controlls/FirstController.java
+++ src/main/java/ru/vertiagina/springcourse/config/controlls/FirstController.java
@@ -1,8 +1,11 @@
 package ru.vertiagina.springcourse.config.controlls;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Author irkin
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("")
-    public String greeting(){
+    public String greeting(HttpServletRequest request){
+        System.out.println( request.getParameter("name") );
+
         return "first/hello";
     }
 
