1c2fd3c73c70e7ec9757cc05b78fb2c7497c03ab
Irina Vertiagina <irkinson9@gmail.com>
2020-12-11 17:05:52 -0500

    calculator add

--- src/main/java/ru/vertiagina/springcourse/config/controlls/FirstController.java
+++ src/main/java/ru/vertiagina/springcourse/config/controlls/FirstController.java
@@ -2,6 +2,7 @@ package ru.vertiagina.springcourse.config.controlls;
 
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +34,17 @@ public class FirstController {
     {
         System.out.println(name);
         return "first/goodbye";
+    }
+
+
+    @GetMapping("calculate")
+    public String calculate(@RequestParam int a, @RequestParam int b, Model model)
+    {
+        int result = a +b;
+     String s = Integer.toString(result);
+        model.addAttribute("result",s);
+       // System.out.println(result);
+        return "first/result";
     }
 
 }
